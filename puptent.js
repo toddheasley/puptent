8a964da4b8bb242cb2092b5d6ed6e38bd705dfac
Todd Heasley <toddheasley@me.com>
2012-03-30 22:52:33 -0400

    Update CSS

--- puptent.js
+++ puptent.js
@@ -1,3 +1,11 @@
+
+/* ----------------------------------------
+    
+    Pup Tent
+    (c) 2012 @toddheasley
+    
+---------------------------------------- */
+
 (function($) {
     var request = function(screenName, count) {
         var url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&screen_name=" + screenName;
@@ -6,7 +14,6 @@
         }
         return url += "&callback=?";
     };
-    
     var properties = {
         screenName: "",
         hashtag: "",
@@ -15,7 +22,6 @@
         profileImageURL: "",
         items: {}
     };
-    
     var methods = {
         profileURL: function() {
             return properties.profileURL + properties.screenName;
@@ -27,6 +33,24 @@
             return false;
         },
         init: function(callback) {
+            var timer;
+            var maxRequests = 3;
+            var requests = 0;
+            var requestData = function() {
+                var path = window.location.pathname.split("/");
+                timer = setTimeout(handleTimeout, 5000);
+                if (path[1].length > 0) {
+                    properties.screenName = path[1];
+                    if (path.length > 2) {
+                        properties.hashtag = path[2];
+                        $.getJSON(request(properties.screenName, 150), function(data) {
+                            clearTimeout(timer);
+                            handleData(data);
+                        });
+                    }
+                }
+                requests++;
+            };
             var handleData = function(data) {
                 var filterItems = function(data) {
                     var filteredItems = [];
@@ -39,65 +63,48 @@
                     }
                     return filteredItems;
                 };
-                
                 var items = filterItems(data);
-                
                 if (items.length > 0) {
                     for (item in items) {
                         if (properties.profileImageURL.length < 1) {
                             properties.profileImageURL = items[item].user.profile_image_url.replace("_normal", "_bigger");
                         }
-                        
-                        var name = "";
-                        
+                        var name = "untitled";
                         var element = {
                             mediaURL: "",
                             text: items[item].text.replace("#" + properties.hashtag, ""),
                             urls: []
                         }
-                        
                         for (hashtag in items[item].entities.hashtags) {
                             if (items[item].entities.hashtags[hashtag].text != properties.hashtag) {
                                 name = items[item].entities.hashtags[hashtag].text;
                                 element.text = element.text.replace("#" + name, "");
                             }
                         }
-                        
                         if (typeof items[item].entities.media != "undefined") {
                             element.mediaURL = items[item].entities.media[0].media_url + properties.mediaSize;
                             element.text = element.text.replace(items[item].entities.media[0].url, "");
                         }
-                        
                         for (url in items[item].entities.urls) {
                             element.urls.push(items[item].entities.urls[url].url);
                         }
-                        
                         element.text = $.trim(element.text);
-                        
                         if (properties.items[name] == null) {
                             properties.items[name] = [];
                         }
                         properties.items[name].push(element);
                     }
                 }
-                
-                if (callback != null) {
+                callback();
+            };
+            var handleTimeout = function() {
+                if (requests < maxRequests) {
+                    requestData();
+                } else {
                     callback();
                 }
-            };
-            
-            var timer = setTimeout(callback, 5000);
-            var path = window.location.pathname.split("/");
-            if (path[1].length > 0) {
-                properties.screenName = path[1];
-                if (path.length > 2) {
-                    properties.hashtag = path[2];
-                    $.getJSON(request(properties.screenName, 200), function(data) {
-                        clearTimeout(timer);
-                        handleData(data);
-                    });
-                }
             }
+            requestData();
         }
     };
     
