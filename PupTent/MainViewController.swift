52b8efc9735b88189e5ebad771cef2c7316b45c6
Todd Heasley <toddheasley@me.com>
2015-03-02 17:26:47 -0500

    Rough in MainViewController; refactor AppDelegate

---
+++ PupTent/MainViewController.swift
@@ -0,0 +1,161 @@
+//
+//  MainViewController.swift
+//  PupTent
+//
+//  (c) 2015 @toddheasley
+//
+
+import Cocoa
+import PupKit
+
+class MainViewController: NSViewController {
+    @IBOutlet weak var previewButton: NSButton?
+    @IBOutlet weak var makeNewSiteButton: NSButton?
+    @IBOutlet weak var openSiteButton: NSButton?
+    
+    @IBAction func preview(sender: AnyObject?) {
+        if let manager = self.manager {
+            var URI = manager.site.URI
+            NSWorkspace.sharedWorkspace().openFile(manager.path + URI)
+        }
+    }
+    
+    @IBAction func forget(sender: AnyObject?) {
+        self.toggleEmpty(true, animated: true)
+        
+        NSUserDefaults.standardUserDefaults().path = ""
+        self.manager = nil
+    }
+    
+    @IBAction func makeNewSite(sender: AnyObject?) {
+        var openPanel: NSOpenPanel = NSOpenPanel()
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.title = "Choose an Empty Folder..."
+        openPanel.prompt = "Use This Folder"
+        openPanel.beginWithCompletionHandler( { (result) -> Void in
+            if result != NSFileHandlingPanelOKButton {
+                return
+            }
+            let path = openPanel.URL!.path! + "/"
+            if let error = Manager.pitch(path) as NSError! {
+                self.showAlert("\(error.localizedDescription)")
+                return
+            }
+            self.openSite(path, animated: true)
+        })
+    }
+    
+    @IBAction func openExistingSite(sender: AnyObject?) {
+        var openPanel: NSOpenPanel = NSOpenPanel()
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.title = "Choose an Existing Site..."
+        openPanel.beginWithCompletionHandler( { (result) -> Void in
+            if result != NSFileHandlingPanelOKButton {
+                return
+            }
+            self.openSite(openPanel.URL!.path! + "/", animated: true)
+        })
+    }
+    
+    @IBAction func makeNewPage(sender: AnyObject?) {
+        
+    }
+    
+    @IBAction func deletePage(sender: AnyObject?) {
+        
+    }
+    
+    var manager: Manager?
+    var canForget: Bool {
+        get {
+            return !NSUserDefaults.standardUserDefaults().path.isEmpty
+        }
+    }
+    var canDeletePage: Bool {
+        get {
+            return false
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let delegate = NSApplication.sharedApplication().delegate as? AppDelegate {
+            delegate.mainViewController = self
+        }
+        
+        self.toggleEmpty(true, animated: false)
+        if (self.canForget) {
+            
+            // Open most recent site
+            self.openSite(NSUserDefaults.standardUserDefaults().path, animated: false)
+        }
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        // Remove preview button from view
+        self.previewButton?.removeFromSuperview()
+        
+        if let window = self.view.window as? Window, view = window.titleBarView as NSView! {
+            
+            // Insert preview button into window title bar
+            view.addSubview(self.previewButton!)
+            
+            var frame = self.previewButton!.frame
+            frame.origin.x = view.bounds.size.width - frame.size.width - 3.0
+            frame.origin.y = 12.0
+            self.previewButton!.frame = frame
+            
+            self.previewButton!.translatesAutoresizingMaskIntoConstraints = true
+        }
+    }
+    
+    private func openSite(path: String, animated: Bool) {
+        var error: NSError?
+        self.manager = Manager(path: path, error: &error)
+        if (error != nil) {
+            self.showAlert("\(error!.localizedDescription)")
+            return
+        }
+        
+        // Remember path
+        NSUserDefaults.standardUserDefaults().path = self.manager!.path
+        
+        // TODO: Implement site view controller; fill in values
+        
+        
+        self.toggleEmpty(false, animated: true)
+    }
+    
+    private func toggleEmpty(empty: Bool, animated: Bool) {
+        self.previewButton!.hidden = empty
+        
+        self.makeNewSiteButton!.hidden = !empty
+        self.openSiteButton!.hidden = !empty
+    }
+    
+    private func showAlert(text: String) {
+        
+        // TODO: Implement custom alert view
+        println("Alert: \(text)")
+    }
+}
+
+extension NSUserDefaults {
+    var path: String {
+        set {
+            NSUserDefaults.standardUserDefaults().setObject(newValue as NSString, forKey: "path")
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+        get {
+            if let path: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("path") {
+                return path as! String
+            }
+            return ""
+        }
+    }
+}
