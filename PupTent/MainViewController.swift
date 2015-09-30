346a19e37453404d6015dbb0b843399386da74db
Todd Heasley <toddheasley@me.com>
2015-09-30 13:46:47 -0400

    Strip PupTent UI; update app icon

--- PupTent/MainViewController.swift
+++ PupTent/MainViewController.swift
@@ -10,99 +10,29 @@ import PupKit
 
 class MainViewController: NSViewController {
     var siteViewController: SiteViewController?
+    
     var canForget: Bool {
-        get {
-            return !NSUserDefaults.standardUserDefaults().path.isEmpty
-        }
-    }
-    var empty: Bool {
-        get {
-            if let emptyView = self.emptyView {
-                return !emptyView.hidden
-            }
-            return true
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let delegate = NSApplication.sharedApplication().delegate as? AppDelegate {
-            delegate.mainViewController = self
-        }
-        
-        self.emptyView!.wantsLayer = true
-        self.emptyView!.layer?.backgroundColor = NSColor.whiteColor().CGColor
-        
-        self.siteViewController = SiteViewController(nibName: "Site", bundle: nil)
-        self.view.addSubview(self.siteViewController!.view)
-        
-        // Default to empty state
-        self.toggleEmpty(true, animated: false)
-        if (self.canForget) {
-            
-            // Open most recent site
-            self.openSite(NSUserDefaults.standardUserDefaults().path, animated: false)
-        }
-    }
-    
-    override func viewWillAppear() {
-        super.viewWillAppear()
-        self.toggleEmpty(self.empty, animated: false)
+        return !NSUserDefaults.standardUserDefaults().path.isEmpty
     }
     
     private func openSite(path: String, animated: Bool) {
-        var error: NSError?
-        let manager: Manager?
         do {
-            manager = try Manager(path: path)
-        } catch let error1 as NSError {
-            error = error1
-            manager = nil
-        }
-        if (error != nil) {
-            self.alert(error!.localizedDescription)
+            let manager = try Manager(path: path)
+            
+            // Remember path
+            NSUserDefaults.standardUserDefaults().path = manager.path
+        } catch {
+            print((error as NSError).localizedDescription)
             return
         }
-        
-        // Remember path
-        NSUserDefaults.standardUserDefaults().path = manager!.path
-        
-        // Configure site view controller
-        self.siteViewController!.manager = manager!
-        self.toggleEmpty(false, animated: true)
     }
-    
-    private func toggleEmpty(empty: Bool, animated: Bool) {
-        if let window = self.view.window as? Window {
-            window.toggleToolbar(empty)
-        }
-        self.siteViewController!.view.hidden = empty
-        self.emptyView!.hidden = !empty
-    }
-    
-    private func alert(text: String) {
-        self.alertLabel?.hidden = text.isEmpty
-        self.alertLabel?.stringValue = "\(text)"
-        5.0.delay {
-            self.alertLabel?.hidden = true
-            self.alertLabel?.stringValue = ""
-        }
-    }
-    
-    // MARK: IBOutlet, IBAction
-    @IBOutlet weak var makeNewSiteButton: NSButton?
-    @IBOutlet weak var openSiteButton: NSButton?
-    @IBOutlet weak var alertLabel: NSTextField?
-    @IBOutlet weak var emptyView: NSView?
     
     @IBAction func forget(sender: AnyObject?) {
-        self.toggleEmpty(true, animated: true)
         NSUserDefaults.standardUserDefaults().path = ""
-        self.siteViewController?.manager = nil
     }
     
     @IBAction func makeNewSite(sender: AnyObject?) {
-        var openPanel: NSOpenPanel = NSOpenPanel()
+        let openPanel: NSOpenPanel = NSOpenPanel()
         openPanel.canChooseDirectories = true
         openPanel.canCreateDirectories = true
         openPanel.canChooseFiles = false
@@ -113,11 +43,12 @@ class MainViewController: NSViewController {
                 return
             }
             let path = openPanel.URL!.path! + "/"
-            if let error = Manager.pitch(path) as NSError! {
-                self.alert(error.localizedDescription)
-                return
+            do {
+                try Manager.pitch(path)
+                self.openSite(path, animated: true)
+            } catch {
+                print((error as NSError).localizedDescription)
             }
-            self.openSite(path, animated: true)
         })
     }
     
@@ -132,6 +63,24 @@ class MainViewController: NSViewController {
             }
             self.openSite(openPanel.URL!.path! + "/", animated: true)
         })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let delegate = NSApplication.sharedApplication().delegate as? AppDelegate {
+            delegate.mainViewController = self
+        }
+        
+        if (canForget) {
+            
+            // Open most recent site
+            openSite(NSUserDefaults.standardUserDefaults().path, animated: false)
+        }
+    }
+    
+    override func viewWillAppear() {
+        view.window?.toolbarHidden = true
     }
 }
 
--- PupTent/Window.swift
+++ PupTent/Window.swift
@@ -8,26 +8,31 @@
 import Cocoa
 
 class Window: NSWindow {
-    var titleBarView: NSView? {
-        get {
-            if let view = self.contentView.superview, subviews = view.subviews {
-                return subviews[1] as? NSView
-            }
-            return nil
-        }
-    }
-    
-    func toggleToolbar(hidden: Bool) {
-        self.toolbar?.showsBaselineSeparator = !hidden
-        self.titlebarAppearsTransparent = hidden
-    }
-    
     override init(contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, `defer` flag: Bool) {
         super.init(contentRect: contentRect, styleMask: aStyle, backing: bufferingType, `defer`: flag)
-        self.titleVisibility = NSWindowTitleVisibility.Hidden
+        titleVisibility = NSWindowTitleVisibility.Hidden
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+}
+
+extension NSWindow {
+    var titleBar: NSView? {
+        guard let _ = contentView, _ = contentView!.superview else {
+            return nil
+        }
+        return contentView!.superview!.subviews[1]
+    }
+    
+    var toolbarHidden: Bool {
+        set {
+            toolbar?.showsBaselineSeparator = !newValue
+            titlebarAppearsTransparent = newValue
+        }
+        get {
+            return titlebarAppearsTransparent
+        }
     }
 }
